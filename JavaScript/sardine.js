fec509c1915412071a6da5fc122e460829fd687e
nicolecarter-23 <ntc.carter@gmail.com>
2026-02-16 18:34:50 -0500

    js and css changes

--- JavaScript/sardine.js
+++ JavaScript/sardine.js
@@ -1,14 +1,35 @@
 window.onload = function(){
-        const sardineTin = document.getElementById("sardine-can")
+    const sardineTin = document.getElementById("sardine-can");
+    const sardineCount = this.document.getElementById("sardine-count");
 
-        sardineTin.style.transition = "transform 0.2s";
-        sardineTin.style.cursor = "pointer";
+    let count = 0;
 
-        sardineTin.addEventListener('click', function() {
-            sardineTin.style.transform = "scale(1.1)";
+    sardineTin.style.transition = "transform 0.2s";
+    sardineTin.style.cursor = "pointer";
 
-        setTimeout(function() {
-            sardineTin.style.transform = "scale(1)";
-        }, 200);
+    sardineTin.addEventListener("click", function() {
+        sardineTin.style.transform = "scale(1.1)";
+
+    setTimeout(function() {
+        sardineTin.style.transform = "scale(1)";
+    }, 100);
+
+    count += 1;
+    sardineCount.textContent = count;
     });
+    
+    const shopItems = document.querySelectorAll(".shelf");
+
+    for (i=0; i<shopItems.length; i++){
+        const item = shopItems[i];
+        const shopCount = item.querySelector('.upgrade-count');
+        let upgradeCounter = 0;
+        item.style.cursor = "pointer";
+
+        item.addEventListener("click", function(){
+            upgradeCounter += 1;
+            shopCount.textContent = upgradeCounter;
+        })
+    }
+
 };
