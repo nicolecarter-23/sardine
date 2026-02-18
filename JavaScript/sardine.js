53998afb31d10c0e9ad86633e27c90641d67011b
nicolecarter-23 <ntc.carter@gmail.com>
2026-02-18 10:48:14 -0500

    added js for automatic counter

--- JavaScript/sardine.js
+++ JavaScript/sardine.js
@@ -3,6 +3,8 @@ window.onload = function(){
     const sardineCount = this.document.getElementById("sardine-count");
 
     let count = 0;
+    let sardinesPerClick = 1;
+    let sardinesPerSecond = 0;
 
     sardineTin.style.transition = "transform 0.2s";
     sardineTin.style.cursor = "pointer";
@@ -10,26 +12,58 @@ window.onload = function(){
     sardineTin.addEventListener("click", function() {
         sardineTin.style.transform = "scale(1.1)";
 
-    setTimeout(function() {
-        sardineTin.style.transform = "scale(1)";
-    }, 100);
+        setTimeout(function() {
+            sardineTin.style.transform = "scale(1)";
+        }, 50);
 
-    count += 1;
-    sardineCount.textContent = count;
+        count += sardinesPerClick;
+        sardineCount.textContent = count;
     });
+
+    setInterval(function() {
+        count += sardinesPerSecond;
+        sardineCount.textContent = count;
+    }, 1000);
     
     const shopItems = document.querySelectorAll(".shelf");
 
     for (i=0; i<shopItems.length; i++){
         const item = shopItems[i];
         const shopCount = item.querySelector('.upgrade-count');
+        const upgradeName = item.querySelector("h2").textContent;
+
         let upgradeCounter = 0;
         item.style.cursor = "pointer";
 
         item.addEventListener("click", function(){
             upgradeCounter += 1;
             shopCount.textContent = upgradeCounter;
+
+            if (upgradeName === "Extra Paw") {
+                sardinesPerClick += 1;
+            }
+            if (upgradeName === "Can Opener") {
+                sardinesPerClick += 5;
+            }
+            if (upgradeName === "Bigger Tin") {
+                sardinesPerClick += 10;
+            }
+
+            if (upgradeName === "Lazy Cat") {
+                sardinesPerSecond += 1;
+            }
+            if (upgradeName === "Regular Cat") {
+                sardinesPerSecond += 10;
+            }
+            if (upgradeName === "Rich Cat") {
+                sardinesPerSecond += 1;
+            }
+            if (upgradeName === "Fat Cat") {
+                sardinesPerSecond += 25;
+            }
         })
     }
+
+
 
 };
